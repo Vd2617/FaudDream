0b7d6d5d2545c3799ef7da807058db0b17ae3747
VD <VD@DESKTOP-0QB29K5>
2020-06-01 12:49:12 +0300

    add drug&drop for images

--- WebFundDream/Controllers/CompanyController.cs
+++ WebFundDream/Controllers/CompanyController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using WebFundDream.Models;
 
@@ -11,8 +15,12 @@ namespace WebFundDream.Controllers
     public class CompanyController : Controller
     {
         private CompanyContext db;
-        public CompanyController(CompanyContext companyContext) {
+        IWebHostEnvironment _appEnvironment;
+        public CompanyController(CompanyContext companyContext, IWebHostEnvironment appEnvironment)
+        {
             db = companyContext;
+            _appEnvironment = appEnvironment;
+
         }
         public async Task<IActionResult> ShowCompany()
         {
@@ -28,6 +36,26 @@ namespace WebFundDream.Controllers
             db.Companies.Add(company);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Upload(IList<IFormFile> file)
+        {
+            Random rnd = new Random();
+            int value = rnd.Next();
+            var filename =  value.ToString() + ".jpg";
+            foreach (var item in file)
+            {
+                if (item.Length > 0)
+                {
+                    var filePath = _appEnvironment.WebRootPath + "\\uploads\\" +  filename;
+                  
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await item.CopyToAsync(fileStream);
+                    }
+                }
+            }
+            return StatusCode(200);
         }
     }
 }
